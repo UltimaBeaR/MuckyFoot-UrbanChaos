987d760654a55e195e7161915c5fc8761404235e
ultimabear <thebear@yandex.ru>
2024-04-26 01:20:52 +0300

    removed unused editor headers

--- src/fallen/Editor/Headers/game.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "macros.h"
-#include "..\headers\inline.h"
-// turn off "integral value may be truncated" C++ warnings
-#ifndef _MSC_VER
-#pragma warning 389 9
-#pragma warning 14 9
-#endif
-

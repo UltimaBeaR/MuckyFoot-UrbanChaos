bd095c045948152afe62481bc3549ef7f480e717
ultimabear <thebear@yandex.ru>
2024-04-27 02:10:21 +0300

    pragma once and removed strange compile options from specific cpp files

--- src/fallen/Headers/build2.h
+++ src/fallen/Headers/build2.h
@@ -2,8 +2,7 @@
 // The other building file!  A simpler map...
 //
 
-#ifndef _BUILD2_
-#define _BUILD2_
+#pragma once
 
 //
 // Puts all the facets and walkable faces on the mapwho.
@@ -24,5 +23,3 @@ void remove_walkable_from_map(SLONG face);
 //
 
 void add_facet_to_map(SLONG dfacet);
-
-#endif
